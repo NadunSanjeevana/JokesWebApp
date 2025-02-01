d577e629fa440ee74e2d011513fa79b8a76064fe
Nadun Sanjeevana <108867149+NadunSanjeevana@users.noreply.github.com>
2025-02-01 23:48:07 +0530

    implement Joke model

---
+++ JokesWebApp/Models/Jokes.cs
@@ -0,0 +1,10 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+        public Jokes() { }
+    }
+}
